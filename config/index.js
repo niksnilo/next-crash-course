c5b15262104d22468333c0ec8b76632e48333908
niksnilo <businessniks26@gmail.com>
2021-03-17 10:32:43 +0800

    Changed url server

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://next-crash-course.vercel.app";
+  : "https://next-crash-course-niks.vercel.app";
